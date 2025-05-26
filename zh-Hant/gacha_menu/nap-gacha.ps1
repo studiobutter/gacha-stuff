2e89f7705e389cb4ac1b58a93a1be19c27024ade
studiobutter <bebosua05@yahoo.com.vn>
2025-05-26 12:15:16 +0700

    Add Gacha link retrieval scripts for multiple regions and cloud options
    - Implemented hk4e-gacha.ps1 for Global, China, and Cloud Gacha links.
    - Created hkrpg-gacha.ps1 for Global and China Gacha links.
    - Developed nap-gacha.ps1 for Global, China, and Cloud Gacha links.
    - Added gacha_cloud_hk4e.ps1 for copying Global Gacha URL from logs.
    - Introduced gacha_cloud_nap.ps1 for copying China Gacha URL from logs.
    - Created get_signal_link_cn.ps1 and get_signal_link_os.ps1 for retrieving signal links.
    - Implemented get_warp_link_cn.ps1 and get_warp_link_os.ps1 for Warp URL retrieval.
    - Forked and modified getlink.ps1 for improved log file handling and URL extraction.

---
+++ zh-Hant/gacha_menu/nap-gacha.ps1
@@ -0,0 +1,39 @@
+Clear-Host
+function Show-Menu {
+    Write-Host "Make sure to open the Gacha Page before choosing any of these options"
+    Write-Host "Select which Gacha link to obtain:"
+    Write-Host "1. Global"
+    Write-Host "2. China"
+    Write-Host "3. Cloud"
+    Write-Host "Press Ctrl + C to exit"
+}
+
+function Get-Gacha_os {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object Net.WebClient).DownloadString("https://github.com/studiobutter/gacha-stuff/raw/refs/heads/main/gacha_clipboard/get_signal_link_os.ps1")
+    Write-Host "Press any key to return to the menu, or Ctrl + C to exit"
+}
+
+function Get-Gacha_cn {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object Net.WebClient).DownloadString("https://github.com/studiobutter/gacha-stuff/raw/refs/heads/main/gacha_clipboard/get_signal_link_cn.ps1")
+    Write-Host "Press any key to return to the menu, or Ctrl + C to exit"
+}
+
+function Get-Gacha_Cloud {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object Net.WebClient).DownloadString("https://github.com/studiobutter/gacha-stuff/raw/refs/heads/main/gacha_clipboard/gacha_cloud_nap.ps1")
+    Write-Host "Press any key to return to the menu, or Ctrl + C to exit"
+}
+
+while ($true) {
+    Show-Menu
+    $choice = Read-Host "Enter your choice"
+    
+    switch ($choice) {
+        1 { Get-Gacha_os }
+        2 { Get-Gacha_cn }
+        3 { Get-Gacha_Cloud }
+        default { Write-Host "Invalid choice. Please try again." }
+    }
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    Clear-Host
+}
+
