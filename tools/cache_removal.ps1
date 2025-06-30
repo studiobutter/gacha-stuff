ce84ec4c529138def4a48970059c4e66590d2a86
studiobutter <bebosua05@yahoo.com.vn>
2025-07-01 01:53:04 +0700

    feat: Add cache removal tool with launcher detection options and localized messages

--- tools/cache_removal.ps1
+++ tools/cache_removal.ps1
@@ -23,19 +23,42 @@ function Get-ScriptUrl {
     
 function Show-Menu {
     Write-Host $Locale.ToolSelection
-    foreach ($option in $Locale.ToolOptions) {
+    foreach ($option in $Locale.CleanerOptions) {
         Write-Host $option
     }
 }
 
-function Get-Cleaner {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex "&{$((New-Object System.Net.WebClient).DownloadString($(Get-ScriptUrl "tools/cache_removal.ps1")))}"
+function Invoke-NoLauncher {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex "&{$((New-Object System.Net.WebClient).DownloadString($(Get-ScriptUrl "tools/cache_removal/no_launcher.ps1")))}"
 }
+
+function Invoke-HoYoPlay {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex "&{$((New-Object System.Net.WebClient).DownloadString($(Get-ScriptUrl "tools/cache_removal/hyp.ps1")))}"
+}
+
 
 function Close-Clear {
     Write-Host $Locale.GachaMenuExit -ForegroundColor Yellow
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex "&{$((New-Object System.Net.WebClient).DownloadString($(Get-ScriptUrl "cleanup.ps1")))}"
     exit 0
+}
+
+function Show-More {
+    param (
+        [string]$OptionalParameters
+    )
+    Write-Host $Locale.ToolCleanerDisclaimer1
+    Write-Host $Locale.ToolCleanerDisclaimer2
+    Write-Host $Locale.ToolCleanerDisclaimer3
+    Write-Host $Locale.ToolCleanerDisclaimer4
+    Write-Host $Locale.ToolCleanerDisclaimer5
+    Write-Host $Locale.ToolCleanerDisclaimer6
+    Write-Host $Locale.ToolCleanerDisclaimer7
+    Write-Host $Locale.ToolCleanerDisclaimerLink
+    Write-Host $Locale.ToolCleanerAnyKey -ForegroundColor Yellow
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    Clear-Host
+    Show-Menu
 }
 
 while ($true) {
@@ -44,7 +67,9 @@ while ($true) {
     
     switch ($choice) {
         0 { Close-Clear }
-        1 { Get-Cleaner }
+        1 { Invoke-NoLauncher }
+        2 { Invoke-HoYoPlay }
+        ? { Show-More }
         default { Write-Host $Locale.InvalidChoice -ForegroundColor Red; continue }
     }
     
