b97c1e649a0942a06a68bd3676074e0f81eb3adf
studiobutter <bebosua05@yahoo.com.vn>
2025-07-14 00:29:54 +0700

    refactor: Simplify Show-Menu function and remove unused code for cleaner execution flow

--- tools/cache_removal.ps1
+++ tools/cache_removal.ps1
@@ -1,81 +1,71 @@
-# This is where all the tools will be shown to the user
-# This is to correspond to Issue #3
-
-$gachaLogTmp = "$env:TMP\gacha-log"
-
-Import-LocalizedData -BaseDirectory $gachaLogTmp -FileName 'Gacha.Resources.psd1' -BindingVariable Locale
-
-Clear-Host
-[Console]::Title = $Locale.GachaMenuTitle
-
-function Get-ScriptUrl {
-    param([string]$ScriptPath)
-    
-    $isLocalTesting = $env:GACHA_LOCAL_TEST -eq "true"
-    if ($isLocalTesting) {
-        $localPath = Join-Path $env:GACHA_LOCAL_PATH $ScriptPath
-        return "file:///$($localPath.Replace('\', '/'))"
-    }
-    else {
-        return "https://raw.githubusercontent.com/studiobutter/gacha-stuff/refs/heads/main/$ScriptPath"
-    }
-}
-    
 function Show-Menu {
-    Write-Host $Locale.ToolSelection
-    foreach ($option in $Locale.CleanerOptions) {
-        Write-Host $option
-    }
-}
-
-function Invoke-NoLauncher {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex "&{$((New-Object System.Net.WebClient).DownloadString($(Get-ScriptUrl "tools/cache_removal/no_launcher.ps1")))}"
-}
-
-function Invoke-HoYoPlay {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex "&{$((New-Object System.Net.WebClient).DownloadString($(Get-ScriptUrl "tools/cache_removal/hyp.ps1")))}"
-}
-
-
-function Close-Clear {
-    Write-Host $Locale.GachaMenuExit -ForegroundColor Yellow
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex "&{$((New-Object System.Net.WebClient).DownloadString($(Get-ScriptUrl "cleanup.ps1")))}"
-    exit 0
-}
-
-function Show-More {
     param (
-        [string]$OptionalParameters
+        [bool]$hoyoplayEnabled,
+        [bool]$collapseEnabled
     )
-    Write-Host $Locale.ToolCleanerDisclaimer1
-    Write-Host $Locale.ToolCleanerDisclaimer2
-    Write-Host $Locale.ToolCleanerDisclaimer3
-    Write-Host $Locale.ToolCleanerDisclaimer4
-    Write-Host $Locale.ToolCleanerDisclaimer5
-    Write-Host $Locale.ToolCleanerDisclaimer6
-    Write-Host $Locale.ToolCleanerDisclaimer7
-    Write-Host $Locale.ToolCleanerDisclaimerLink
-    Write-Host $Locale.ToolCleanerAnyKey -ForegroundColor Yellow
-    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-    Clear-Host
-    Show-Menu
+    Write-Host $Locale.ToolSelection
+    Write-Host $Locale.CleanerOptions[0] # 0: Exit
+    Write-Host $Locale.CleanerOptions[1] # 1: No Launcher
+    if ($hoyoplayEnabled) {
+        Write-Host $Locale.CleanerOptions[2] # 2: HoYoPlay
+    } else {
+        Write-Host "$($Locale.CleanerOptions[2]) [$($Locale.Disabled)]" -ForegroundColor DarkGray
+    }
+    if ($collapseEnabled) {
+        Write-Host $Locale.CleanerOptions[3] # 3: CollapseLauncher
+    } else {
+        Write-Host "$($Locale.CleanerOptions[3]) [$($Locale.Disabled)]" -ForegroundColor DarkGray
+    }
+    Write-Host $Locale.CleanerOptions[4] # ?: More Info
 }
 
+# Main loop
 while ($true) {
-    Show-Menu
+    # Re-run checks for each loop
+    $regBase = 'HKCU:\Software\Classes'
+    $regFolders = @(
+        'HYP-cn-1-1',
+        'HYP-cn-1-2',
+        'HYP-cn-14-0-hk4e-cn-umfgRO5gh5',
+        'HYP-cn-14-0-hkrpg-cn-6P5gHMNyK3',
+        'HYP-cn-14-0-nap-cn-xV0f4r1GT0',
+        'HYP-global-1-0',
+        'HYP-global-1-6-hk4e-global-8fANlj5K7I'
+    )
+    $hoyoplayEnabled = $false
+    foreach ($folder in $regFolders) {
+        if (Test-Path (Join-Path $regBase $folder)) {
+            $hoyoplayEnabled = $true
+            break
+        }
+    }
+    $collapsePath = Join-Path "$env:USERPROFILE\AppData\LocalLow" 'CollapseLauncher'
+    $collapseEnabled = Test-Path $collapsePath
+
+    Show-Menu -hoyoplayEnabled $hoyoplayEnabled -collapseEnabled $collapseEnabled
     $choice = Read-Host $Locale.EnterChoice
-    
-    switch ($choice) {
-        0 { Close-Clear }
-        1 { Invoke-NoLauncher }
-        2 { Invoke-HoYoPlay }
-        ? { Show-More }
+    $choiceStr = "$choice"
+
+    switch ($choiceStr) {
+        "0" { Close-Clear }
+        "1" { Invoke-NoLauncher }
+        "2" {
+            if ($hoyoplayEnabled) {
+                Invoke-HoYoPlay
+            } else {
+                Write-Host $Locale.DisabledChoice -ForegroundColor Red
+                continue
+            }
+        }
+        "3" {
+            if ($collapseEnabled) {
+                Invoke-Collapse
+            } else {
+                Write-Host $Locale.DisabledChoice -ForegroundColor Red
+                continue
+            }
+        }
+        "?" { Show-More }
         default { Write-Host $Locale.InvalidChoice -ForegroundColor Red; continue }
     }
-    
-    Write-Host $Locale.GachaMenuAnyKey -ForegroundColor Yellow
-    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-    Clear-Host
 }
-
-Write-Host "Exiting"
